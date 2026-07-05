4ab8add28aedcdbc523ef020706355cc1ed9fb99
shougo3358 <yamawakishougo00@gmail.com>
2026-07-05 13:21:17 +0900

    ddd

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -182,7 +182,7 @@
                 {{-- 全員共通 --}}
                 <li class="nav-item">
                     <a class="nav-link active" href="#"
-                    onclick="goGroupRecord()">グループ 的中記録</a>
+                    onclick="goGroupRecord()">グループ 的中記録ooo</a>
                 </li>
 
                 <li class="nav-item">
