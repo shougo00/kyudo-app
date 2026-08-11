ed11b102f75d36c16bcbc60fb87d35846babe15b
shougo3358 <yamawakishougo00@gmail.com>
2026-08-11 14:41:12 +0900

    管理画面修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL; // ← これ忘れずに
 
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Paginator::useBootstrapFive();
+
         // 本番環境のみHTTPSを強制
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
